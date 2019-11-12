0529ab4e86f3e3fb8238a63f70c35970f368ea91
Pavan Kamra <pavankamra081796@gmail.com>
2019-11-11 21:53:54 -0500

    #1: Starting off

---
+++ src/text-parser.js
@@ -0,0 +1,16 @@
+const jsdom = require("jsdom");
+
+const { JSDOM } = jsdom;
+
+//const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+//console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+ 
+exports.run = async function (html) {
+  try {
+    
+  }
+  catch(error){
+    
+  }
+ 
+}
